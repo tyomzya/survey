c6b1c939b7937bcd4aa4ce74079ba73b22a0abe4
tyomzya <tyomzya@mail.ru>
2024-07-29 17:29:17 +0300

    refactoring

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-var answerNode = document.getElementById('answer');
-var sendButtonNode = document.getElementById('sendButton');
-var resultNode = document.getElementById('result');
-sendButtonNode.addEventListener('click', function () {
+var answer = document.getElementById('answer');
+var sendButton = document.getElementById('sendButton');
+var result = document.getElementById('result');
+sendButton.addEventListener('click', function () {
     var currentYear = "2024";
-    if (answerNode.value == '') {
+    if (answer.value == '') {
         return;
     }
-    if (answerNode.value == currentYear) {
-        resultNode.innerHTML = "<div class=\"result-right\">\n        <p class=\"right-font\">\u041E\u0442\u0432\u0435\u0442 \u0432\u0435\u0440\u043D\u044B\u0439! :)</p>\n      </div>";
+    if (answer.value == currentYear) {
+        result.innerHTML = "<div class=\"result-right\">\n        <p class=\"right-font\">\u041E\u0442\u0432\u0435\u0442 \u0432\u0435\u0440\u043D\u044B\u0439! :)</p>\n      </div>";
     }
     else {
-        resultNode.innerHTML = "<div class=\"result-wrong\">\n        <p class=\"wrong-font\">\u041E\u0442\u0432\u0435\u0442 \u043D\u0435\u0432\u0435\u0440\u043D\u044B\u0439 :(</p>\n      </div>";
+        result.innerHTML = "<div class=\"result-wrong\">\n        <p class=\"wrong-font\">\u041E\u0442\u0432\u0435\u0442 \u043D\u0435\u0432\u0435\u0440\u043D\u044B\u0439 :(</p>\n      </div>";
     }
 });
